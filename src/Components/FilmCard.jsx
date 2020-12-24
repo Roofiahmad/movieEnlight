06514857e1d57857a36dd430279d2e0a3e8c4e23
Jody <muhammad.adnand.jody@gmail.com>
2020-12-24 10:17:41 +0700

    push again

--- src/Components/FilmCard.jsx
+++ src/Components/FilmCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import classCss from "./FilmCard.module.css";
+import { Link } from "react-router-dom";
 
 const FilmCard = (props) => {
   return (
     <div className={classCss.card_container}>
+      
       {props.list.map((data, index) => (
+        <Link to="/filmdetails">
         <div className={classCss.card}>
           <img
             className={classCss.card_image}
@@ -14,7 +17,9 @@ const FilmCard = (props) => {
           <h5 className={classCss.title_text}>{data.title}</h5>
           <h6 className={classCss.category_text}>{data.category}</h6>
         </div>
+        </Link>
       ))}
+      
     </div>
   );
 };
