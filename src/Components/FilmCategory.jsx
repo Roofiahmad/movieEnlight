7ef577e55767c605e8d4fc1a6ab378baa77cf846
Roofi-Kun <you@example.com>
2020-12-28 21:36:11 +0700

    nothing

--- src/Components/FilmCategory.jsx
+++ src/Components/FilmCategory.jsx
@@ -1,7 +1,8 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import classCss from "../Components/FilmCategory.module.css";
 import FilmCard from "./FilmCard";
+import axios from "axios";
 
 export default function FilmCategory() {
   const image_size = { width: "200px", height: "300px", borderRadius: "10px" };
@@ -14,6 +15,7 @@ export default function FilmCategory() {
     "comedy",
   ];
   const [category, setCategory] = useState(listCategory);
+  const [data, setData] = useState("");
   const [filmList, setFilmList] = useState([
     {
       title: "Inuyasha",
@@ -38,72 +40,26 @@ export default function FilmCategory() {
       imageUrl:
         "https://i2.wp.com/halcyonrealms.com/blogpics/slamdunknew09.jpg?resize=500%2C780",
     },
-    {
-      title: "Black Clover",
-      category: "Anime",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/Black_Clover%2C_volume_1.jpg/220px-Black_Clover%2C_volume_1.jpg",
-    },
-    {
-      title: "Inuyasha",
-      category: "Anime",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/en/c/c3/InuYasha_The_Final_Act_DVD_volume_1_cover.jpg",
-    },
-    {
-      title: "Nanatsu",
-      category: "Anime",
-      imageUrl:
-        "https://i.pinimg.com/originals/8e/0e/65/8e0e652417ff7780c9027eed4f4eb0a5.jpg",
-    },
-    {
-      title: "Saint Seiya",
-      category: "Anime",
-      imageUrl: "https://static.tvtropes.org/pmwiki/pub/images/main03_5.jpg",
-    },
-    {
-      title: "Slam Dunk",
-      category: "Anime",
-      imageUrl:
-        "https://i2.wp.com/halcyonrealms.com/blogpics/slamdunknew09.jpg?resize=500%2C780",
-    },
-    {
-      title: "Black Clover",
-      category: "Anime",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/Black_Clover%2C_volume_1.jpg/220px-Black_Clover%2C_volume_1.jpg",
-    },
-    {
-      title: "Inuyasha",
-      category: "Anime",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/en/c/c3/InuYasha_The_Final_Act_DVD_volume_1_cover.jpg",
-    },
-    {
-      title: "Nanatsu",
-      category: "Anime",
-      imageUrl:
-        "https://i.pinimg.com/originals/8e/0e/65/8e0e652417ff7780c9027eed4f4eb0a5.jpg",
-    },
-    {
-      title: "Saint Seiya",
-      category: "Anime",
-      imageUrl: "https://static.tvtropes.org/pmwiki/pub/images/main03_5.jpg",
-    },
-    {
-      title: "Slam Dunk",
-      category: "Anime",
-      imageUrl:
-        "https://i2.wp.com/halcyonrealms.com/blogpics/slamdunknew09.jpg?resize=500%2C780",
-    },
-    {
-      title: "Black Clover",
-      category: "Anime",
-      imageUrl:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/6/69/Black_Clover%2C_volume_1.jpg/220px-Black_Clover%2C_volume_1.jpg",
-    },
   ]);
+  useEffect(() => {
+    axios
+      .get(
+        "http://ec2-13-229-61-46.ap-southeast-1.compute.amazonaws.com:6969/movie/?page=1&limit=30"
+      )
+      .then((response) => {
+        console.log(response.data.posts, "GET MOVIE DATA SUCCESS");
+        const newList = response.data.post;
+        console.log(newList);
+        setData({ ...data, data: newList });
+        // let title = response.data.posts;
+        // console.log(response.data.posts, "INI LIST ISI TITLE");
+        // setTitle(title);
+        // localStorage.setItem("movie", "GET");
+        // setFilmList(response.data.posts);
+      });
+  }, []);
 
+  console.log(data, "ini dari API");
   return (
     <div className={classCss.category_container}>
       <p className={classCss.category_text}>Browse by category</p>
