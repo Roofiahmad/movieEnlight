0c6ab2fb1954927ba886aa4b1a6cba663401e3f9
Afif Firdaus <afif.firdaus94@gmail.com>
2020-12-29 08:12:32 +0700

    update

--- src/Components/FilmCard.jsx
+++ src/Components/FilmCard.jsx
@@ -7,13 +7,13 @@ const FilmCard = (props) => {
   const [movieArray, setMovie] = useState("")
   useEffect(() => {
     getData()
-  },[]);
+  }, []);
   function getData() {
-    axios.get( "https://cors-anywhere.herokuapp.com/" +
+    axios.get("https://cors-anywhere.herokuapp.com/" +
       "http://ec2-13-229-61-46.ap-southeast-1.compute.amazonaws.com:6969/movie/?page=1&limit=30",
-      )
-      .then( ( response ) => {
-        console.log( response, "GET MOVIE DATA SUCCESS" );
+    )
+      .then((response) => {
+        console.log(response, "GET MOVIE DATA SUCCESS");
         let title = response.data.posts;
         console.log(response.data.posts, "INI LIST ISI TITLE");
         // setTitle(title);
@@ -23,28 +23,28 @@ const FilmCard = (props) => {
   }
 
   console.log("GUYS INI STATENYAA", movieArray);
-  
+
   return (
-    
+
     <div className={classCss.card_container}>
-    { 
-    movieArray.length > 0 ? movieArray.map(data => {
-      return(
-        <Link to="/filmdetails">
-        <div key={data.id} className={classCss.card}>
-          <img
-            className={classCss.card_image}
-            style={props.image_size}
-            src={data.poster}
-          />
-          <h5 className={classCss.title_text}>{data.title}</h5>
-          <h6 className={classCss.category_text}>{data.genre}</h6>
-        </div>
-        </Link>
-        )
-      })
-    :null}
-      
+      {
+        movieArray.length > 0 ? movieArray.map(data => {
+          return (
+            <Link to="/filmdetails">
+              <div key={data.id} className={classCss.card}>
+                <img
+                  className={classCss.card_image}
+                  style={props.image_size}
+                  src={data.poster}
+                />
+                <h5 className={classCss.title_text}>{data.title}</h5>
+                <h6 className={classCss.category_text}>{data.genre}</h6>
+              </div>
+            </Link>
+          )
+        })
+          : null}
+
     </div>
   );
 };
